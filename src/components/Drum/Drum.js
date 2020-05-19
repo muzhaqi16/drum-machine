46794fbe609b131bcd2310c2b5b08db773cba33c
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-05-19 12:43:06 -0400

    Fully working drum pad

---
+++ src/components/Drum/Drum.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useCallback } from 'react'
+import Key from '../Key/';
+import './style.css';
+
+// const keys = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+import sounds from '../../sounds';
+
+function Drum() {
+    const handleUserKeyPress = useCallback(event => {
+        const { key, keyCode } = event;
+        if (keyCode === 32 || (keyCode >= 65 && keyCode <= 90)) {
+            const keys = Array.from(document.querySelectorAll('.clip'));
+            keys.forEach(pad => {
+                if (pad.getAttribute('id').toLowerCase() === key) {
+                    pad.parentElement.classList.remove("clicked");
+                    setTimeout(() => {
+                        pad.parentElement.classList.add("clicked");
+                    }, 100);
+                    const display = document.getElementById("display");
+                    display.textContent = pad.getAttribute("id");
+                    pad.currentTime = 0;
+                    pad.play()
+                }
+            });
+        }
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleUserKeyPress);
+
+        return () => {
+            window.removeEventListener('keydown', handleUserKeyPress);
+        };
+    }, [handleUserKeyPress]);
+
+    return (
+        <div id="container">
+            {/* {display} */}
+            <div className="row">
+                <span id="display">Start pressing a key to get started</span>
+            </div>
+            <div className="row">
+                <Key color="blue" sound={sounds.boom} letter="Q" />
+                <Key color="yellow" sound={sounds.clap} letter="W" />
+                <Key color="green" sound={sounds.hihat} letter="E" />
+
+            </div>
+            <div className="row">
+                <Key color="pink" sound={sounds.kick} letter="A" />
+                <Key color="blue" sound={sounds.openhat} letter="S" />
+                <Key color="yellow" sound={sounds.ride} letter="D" />
+            </div>
+            <div className="row">
+                <Key color="green" sound={sounds.snare} letter="Z" />
+                <Key color="pink" sound={sounds.tink} letter="X" />
+                <Key color="pink" sound={sounds.tom} letter="C" />
+            </div>
+        </div>
+    )
+}
+
+export default Drum
---
+++ src/components/Key/Key.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types';
+
+import './style.css';
+function Key(props) {
+    const [click, setClick] = useState(false);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setClick(false);
+        }, 100);
+        return () => clearTimeout(timer);
+    }, [click]);
+
+    const handleClick = (e) => {
+        const audio = e.target.children[0];
+        if (audio) {
+            audio.currentTime = 0;
+            audio.play();
+            const display = document.getElementById("display");
+            display.textContent = audio.getAttribute("id");
+        }
+        setClick(true);
+    }
+
+    return (
+        <div className="column">
+            <div id={props.letter} className={'drum-pad box ' + props.color + (click ? "" : " clicked")} onClick={handleClick}>
+                <audio className="clip" id={props.letter}
+                    src={props.sound}>
+                    Your browser does not support the
+                    <code>audio</code> element.
+                </audio>
+                {props.letter}
+            </div>
+        </div>
+    )
+}
+Key.propTypes = {
+    color: PropTypes.string
+}
+Key.defaultProps = {
+    color: 'green'
+}
+export default Key
